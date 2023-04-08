7e32d171586d6e8149d2f022415bf29c8359ac52
Sneha Gupta <guptasneha22121999@gmail.com>
2023-04-08 15:18:06 +0530

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,13 +37,13 @@ const NavBar = () => {
         <ul className='flex flex-col items-left text-left absolute top-0 left-0 w-full h-screen bg-stone-800 text-white'>
           <li><img src={Whitelogo} alt="logo" /></li>
           <li className='cursor-pointer uppercase font-small
-              hover:scale-105 duration-200 py-2 px-20 mt-20 focus:text-yellow-400'><link to="/home">Home</link>Home</li>
+              hover:scale-105 duration-200 py-2 px-20 mt-20 focus:text-yellow-400'><a href="/home">Home</a>Home</li>
           <li className=' cursor-pointer uppercase font-small
-              hover:scale-105 duration-200 py-2 px-20 focus:text-yellow-400'><link to="/areapractice">Areas of Practice</link></li>
+              hover:scale-105 duration-200 py-2 px-20 focus:text-yellow-400'><a href="/areapractice">Areas of Practice</a></li>
           <li className='px-20 py-2 cursor-pointer uppercase font-small
              hover:scale-105 duration-200 focus:text-yellow-400'><a href='/career'>Career</a></li>
           <li className='px-20 py-2 cursor-pointer uppercase font-small
-             hover:scale-105 duration-200 focus:text-yellow-400'><a href='/contactus'>Contact Us</a></li>
+             hover:scale-105 duration-200 focus:text-yellow-400'><a href='/publicationmedia'>Publication And Media</a></li>
 
 
         </ul>
